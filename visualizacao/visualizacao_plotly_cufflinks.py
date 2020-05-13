75d01344304e4de8721ca1a4fa8b3cf1d5af681d
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2020-05-13 10:11:50 -0300

    [UPD] Criação e visualização de gráficos a partir do Plotly

--- visualizacao/visualizacao_plotly_cufflinks.py
+++ visualizacao/visualizacao_plotly_cufflinks.py
@@ -2,49 +2,67 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from plotly import __version__
+
+# Visualizar a versão do plotly
+print(__version__)
+
 # Importar as bibliotecas e utilizar offline
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.offline import download_plotlyjs, plot, iplot
 import cufflinks as cf
 cf.go_offline()
 
-# Plotly Express
-# Interface de alto nível fácil de usar do Plotly que produz figuras
-import plotly.express as px
 
-# Permite visualizar cada ponto, salvar como png, salvar na nuvem, 
-# realizar um zoom, se mover no gráfico
-
-# Dataframe fictício
+# Dataframes fictícios
 df = pd.DataFrame(np.random.randn(100, 4), columns='A B C D'.split())
 print(df.head())
-
 df2 = pd.DataFrame({'Categoria':['A', 'B', 'C'], 'Valores':[32, 43, 50]})
 print(df2.head())
-
-
-# # Gráfico de dispersão utilizando o dataset Iris
-# df_iris = px.data.iris()
-# fig = px.scatter(
-#     df_iris, x='sepal_length', y='sepal_width', color='species', size='petal_length'
-# )
-# fig.show()
-
-# # Gráfico de dispersão
-# fig = px.scatter(df, x='A', y='B')
-# fig.show()
-
-# # Gráfico de barra
-# fig = px.bar(df2, x='Categoria', y='Valores')
-# fig.show()
-
 df3 = pd.DataFrame({'x':[1,2,3,4,5],'y':[10,20,30,20,10],'z':[5,4,3,2,1]})
-df3.iplot(kind='surface',colorscale='rdylbu')
-fig = px.s(df2, x='Categoria', y='Valores')
+print(df3.head())
+
+
+# Visualização normal a partir de métodos embutidos no pandas (Gráfico de dispersão)
+# Gráfico de dispersão = Onde X e Y se encontram é marcado um ponto
+df.plot(kind='scatter', x='A', y='B')
+plt.show()
+
+# Visualização a partir do Plotly (Gráfido de linha)
+fig = df.iplot(asFigure=True, kind='scatter', x='A', y='B')
 fig.show()
 
+# Visualização a partir do Plotly (Gráfido de dispersão)
+fig = df.iplot(asFigure=True, kind='scatter', x='A', y='B', mode='markers')
+fig.show()
 
+# Visualização a partir do Plotly (Gráfido de barra)
+fig = df2.iplot(asFigure=True, kind='bar', x='Categoria', y='Valores')
+fig.show()
 
-# df.iplot(kind='scatter',x='A',y='B',mode='markers',size=10)
+# Visualização a partir do Plotly (Gráfido de barra)
+# Contar quantas vezes possuem elementos nas quatro colunas
+fig = df.count().iplot(asFigure=True, kind='bar')
+fig.show()
 
-# df3 = pd.DataFrame({'x':[1,2,3,4,5],'y':[10,20,30,20,10],'z':[5,4,3,2,1]})
-# df3.iplot(kind='surface',colorscale='rdylbu')
+# Visualização a partir do Plotly (Gráfido de caixa)
+fig = df.iplot(asFigure=True, kind='box')
+fig.show()
+
+# Visualização a partir do Plotly (Gráfido 3D)
+fig = df3.iplot(asFigure=True, kind='surface', colorscale='rdylbu')
+fig.show()
+
+# Visualização a partir do Plotly (Gráfico de diferença)
+# Monitar o desempenho de duas variáveis
+fig = df[['A', 'B']].iplot(asFigure=True, kind='spread')
+fig.show()
+
+# Visualização a partir do Plotly (Gráfico de histograma)
+fig = df[['A']].iplot(asFigure=True, kind='hist', bins=50)
+fig.show()
+
+# Visualização a partir do Plotly (Gráfico de bolha)
+# Gráfico de bolha = É um gráfico de dispersão onde o tamanho das bolhas são
+# determinados em função de uma terceira variável 
+fig = df.iplot(asFigure=True, kind='bubble', x='A', y='B', size='C')
+fig.show()
